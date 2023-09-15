1351cd38f5f9681b63414c651a522ba6ff00b379
Navaid Wahid <144524379+shadow-kage@users.noreply.github.com>
2023-09-15 22:56:54 +0530

    Update matrix_mult.c

--- 500102181/matrix_mult.c
+++ 500102181/matrix_mult.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int n, m, p, q, i, j, k;
+    int n, m, p, q, i, j, k; // n,m are rows and columns of matrix 1 and p,q are rows and columns of matrix 2 & i,j,k are looping contraints
     printf("Enter number of rows and coloumn in matrix 1 : ");
     scanf("%d %d", &n, &m);
     printf("Enter number of rows and coloumn in matrix 2 : ");
     scanf("%d %d", &p, &q);
-    int a[n][m], b[p][q], mul[n][q];
+    int a[n][m], b[p][q], mul[n][q]; // Created matrix 1 and 2 and and multiplication matrix with n rows & q columns
     if (m == p)
     {
         printf("Enter elements of first matrix:\n");
@@ -19,19 +19,19 @@ int main()
             for (j = 0; j < q; j++)
                 scanf("%d", &b[i][j]);
 
-        for (i = 0; i < n; i++)
+        for (i = 0; i < n; i++) // for rows
         {
-            for (j = 0; j < q; j++)
+            for (j = 0; j < q; j++) // for columns
             {
-                mul[i][j] = 0;
-                for (k = 0; k < m; k++)
+                mul[i][j] = 0; // reseting values
+                for (k = 0; k < m; k++) // for multiplication
                 {
                     mul[i][j] = mul[i][j] + a[i][k] * b[k][j];
                 }
             }
         }
 
-        printf("Multiplied matrix is:\n");
+        printf("Multiplied matrix is:\n"); // Printing multiplied matrix
         for (i = 0; i < n; i++)
         {
             for (j = 0; j < q; j++)
@@ -42,6 +42,6 @@ int main()
         }
     }
     else
-        printf("Matrix can't be multiplied");
+        printf("Matrix can't be multiplied"); 
     return 0;
 }
