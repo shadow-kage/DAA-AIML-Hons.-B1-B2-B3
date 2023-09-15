97d906612bbe5716648a3b3567a9eae865b11285
Navaid Wahid <144524379+shadow-kage@users.noreply.github.com>
2023-09-15 22:51:59 +0530

    Update insertion_sort.c

--- 500102181/insertion_sort.c
+++ 500102181/insertion_sort.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void print_array(int *arr, int n)
+void print_array(int *arr, int n) // Function to print array
 {
     for (int i = 0; i < n; i++)
         printf("%d\t", arr[i]);
 }
 
-void insertion_sort(int *arr, int n)
+void insertion_sort(int *arr, int n) // Insertion sort function
 {
     int key, j;
-    for (int i = 1; i <= n - 1; i++)
+    for (int i = 1; i <= n - 1; i++) // Outer loop for numeber of passes
     {
         key = arr[i];
         j = i - 1;
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 && arr[j] > key) // Comparing last element of Sorted subarray and first element of Unsorted subarray
         {
-            arr[j+1] = arr[j];
+            arr[j+1] = arr[j]; // Inserting element
             j--;
         }
-        arr[j+1] = key;
+        arr[j+1] = key; // Shifting element
     }
 }
 
